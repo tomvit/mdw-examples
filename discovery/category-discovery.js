521fd792e159fab287972a4ac1d40b50a9062516
Tomas Vitvar <tomas@vitvar.com>
2011-07-26 17:28:07 +0200

    discovery updated with testing

--- discovery/category-discovery.js
+++ discovery/category-discovery.js
@@ -31,7 +31,7 @@ function discover(goal_cat, directory, taxonomy_root) {
     // check if there exsits a taxonomy element for the goal category
     if (gte !== null) {
         // loop through all service descriptions in the directory
-        for (var i = 0; i < directory.length; i++)
+        for (var i = 0; i < directory.length; i++) 
             // find taxonomy element for the service category
             // in the subtree defined by gte
             if (findTaxonomyElement(directory[i].category_name, gte) !== null)
@@ -42,3 +42,54 @@ function discover(goal_cat, directory, taxonomy_root) {
     // return the result
     return result;
 }
+
+/* following functions may be used to test the category discovery */
+
+var fs = require('fs');
+
+// note that o3-xml is not a standard node.js module;
+// install it with npm install o3-xml
+var xml = require('o3-xml');
+
+// load the taxonomy from xml
+function readTaxonomy(taxonomyFile) {
+    buffer = fs.readFileSync(taxonomyFile, 'utf-8');
+    doc = xml.parseFromString(buffer.toString('utf-8'));
+    
+    var taxonomy_root = {};
+    
+    var readElement = 
+        function(xml_element, taxonomy_element) {
+            taxonomy_element.category_name = xml_element.nodeName;
+            taxonomy_element.categories = [];
+            if (xml_element.childNodes)
+                for (var i = 0; i < xml_element.childNodes.length; i++) {
+                    if (xml_element.childNodes[i].nodeType == 1) { // it is the element type
+                        var e = {};
+                        readElement(xml_element.childNodes[i], e);
+                        taxonomy_element.categories.push(e);
+                    } 
+                }
+        };
+        
+    readElement(doc.documentElement, taxonomy_root);
+    return taxonomy_root;
+}
+
+// you need to change the path for taxonomy.xml file based on
+// location from where you run this js file
+var taxonomy_root = readTaxonomy("./taxonomy.xml");
+
+var directory = [ 
+    { category_name : "order" }, 
+    { category_name : "book" }, 
+    { category_name : "TV", description : "electronic4u" }, 
+    { category_name : "computer", description : "mall.cz/computers" }, 
+    { category_name : "TV", description: "mall.cz/TVs" } ];
+
+var result = discover("electronics", directory, taxonomy_root);
+if (result.length === 0)
+    console.log("No services that match the requested category found!");
+else
+    for (var i = 0; i < result.length; i++)
+        console.log(result[i].description);
