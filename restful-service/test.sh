23632d14f82587586869223d9a3d1c50cf9b21aa
Tomas Vitvar <tomas@vitvar.com>
2011-08-05 12:19:20 +0200

    test.sh minor change

--- restful-service/test.sh
+++ restful-service/test.sh
@@ -4,7 +4,8 @@
 SERVER_URL="http://mdw-examples.tomvit.cloud9ide.com"
 
 # add an order and get the new location; assume everything went ok
-order_uri=$SERVER_URL$(curl -v -X POST $SERVER_URL/orders 2>&1 | awk '/location/ {print $3}')
+order_uri=$SERVER_URL$(curl -v -X POST $SERVER_URL/orders 2>&1 | \
+	awk '/location/ {print $3}')
 
 # remove whitespace
 order_uri=${order_uri//[[:space:]]}
@@ -13,8 +14,8 @@ echo "New order with URI $order_uri has been created"
 
 # add items to the order
 curl -X POST -d "{ \"name\" : \"socks\", \"price\" : 5 }" $order_uri
-curl -X POST -d "{ \"name\" : \"t-shirt\", \"price\" : 20  }" "$order_uri"
-curl -X POST -d "{ \"name\" : \"jumper\", \"price\" : 45  }"  "$order_uri"
+curl -X POST -d "{ \"name\" : \"t-shirt\", \"price\" : 20  }" $order_uri
+curl -X POST -d "{ \"name\" : \"jumper\", \"price\" : 45  }" $order_uri
 
 # close the order
 curl -X PUT -d "{ \"status\" : \"close\" }" $order_uri
